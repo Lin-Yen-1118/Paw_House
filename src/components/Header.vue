5495cc45e0a4b97b6c2b9eed5fce4e05f6220d17
Lin-Yen-1118 <goatruby@gmail.com>
2021-12-29 23:47:08 +0800

    wip

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,87 +2,32 @@
   <header>
     <nav>
       <div class="logo">
-        <router-link to="/"><img id="logo" src="../assets/images/svg/logo.svg" /></router-link>
+        <router-link to="/">
+          <img id="logo" src="../assets/images/svg/logo.svg" />
+        </router-link>
       </div>
       <div class="nav_bar">
         <!-- 用input的checkbox與for特性做漢堡選單的選取器 -->
         <input id="burger" type="checkbox" name="" />
-        <ul id="nav_menu" class="header_bar">
-          <!-- 關於我 -->
-          <li>
-            <router-link to="/aboutUs">
+        <ul class="header_bar">
+          <li v-for="(item, index) of menu" id="nav_menu" :key="index">
+            <router-link :to="item.path">
               <span>
-                <img id="introduction" src="../assets/images/svg/dog01.svg" />
+                <img id="introduction" :src="require(`../assets/images/svg/${item.icon}`)" />
               </span>
-              <span>民宿介紹</span>
+              <span>{{ item.title }}</span>
             </router-link>
-          </li>
-          <!-- 我要認養 -->
-          <li class="adopt_menu">
-            <router-link to="">
-              <span>
-                <img id="adoption" src="../assets/images/svg/cat01.svg" />
-              </span>
-              <span>我要認養</span>
-            </router-link>
-
-            <!-- 我要認養的下拉選單 -->
             <ul class="adopt_menu_select_menu">
-              <li class="adopt_menu_select">
-                <router-link to="/adopt_main" class="link_style"><span>認養須知</span></router-link>
-              </li>
-              <li class="adopt_menu_select">
-                <router-link to="/adopt_dog" class="link_style"><span>認養狗狗</span></router-link>
-              </li>
-              <li class="adopt_menu_select">
-                <router-link to="/adopt_cat" class="link_style"><span>認養貓貓</span></router-link>
-              </li>
-              <li class="adopt_menu_select">
-                <router-link to="/adopt_rabbit" class="link_style">
-                  <span>認養兔兔</span>
-                </router-link>
-              </li>
-              <li class="adopt_menu_select">
-                <router-link to="/adopt_rodent" class="link_style">
-                  <span>認養鼠鼠</span>
-                </router-link>
-              </li>
-              <li class="adopt_menu_select">
-                <router-link to="/" class="link_style"><span>醫療團隊</span></router-link>
-              </li>
-            </ul>
-          </li>
-          <!-- 住宿相關 -->
-          <li class="booking_rooms_menu">
-            <router-link to="">
-              <span>
-                <img id="room" src="../assets/images/svg/rabbit01.svg" />
-              </span>
-              <span>住宿相關</span>
-            </router-link>
-
-            <!-- 住宿的下拉選單 -->
-            <ul class="booking_rooms_menu_select_menu">
-              <li class="booking_rooms_select">
-                <router-link to="/room" class="link_style">
-                  <span>一般住宿</span>
-                </router-link>
-              </li>
-              <li class="booking_rooms_select">
-                <router-link to="/pet_accommodation" class="link_style">
-                  <span>毛孩寄宿</span>
+              <li
+                v-for="(subMenu, subMenuIndex) of item.children"
+                :key="subMenuIndex"
+                class="adopt_menu_select"
+              >
+                <router-link :to="subMenu.path" class="link_style">
+                  <span>{{ subMenu.title }}</span>
                 </router-link>
               </li>
             </ul>
-          </li>
-          <!-- 周邊商品 -->
-          <li>
-            <router-link to="/products" class="link_style">
-              <span>
-                <img id="merchandise" src="../assets/images/svg/mouse01.svg" />
-              </span>
-              <span>周邊商品</span>
-            </router-link>
           </li>
         </ul>
       </div>
@@ -108,6 +53,7 @@
           </li>
         </ul>
       </div>
+
       <!--漢堡選單--->
       <!-- 用input的checkbox與for特性做漢堡選單的選取器 -->
       <label for="burger">
@@ -126,6 +72,67 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'Header',
+  data() {
+    return {
+      menu: [
+        {
+          path: '/aboutUs',
+          icon: 'dog01.svg',
+          title: '民宿介紹',
+          children: []
+        },
+        {
+          path: '/',
+          icon: 'cat01.svg',
+          title: '我要認養',
+          children: [
+            {
+              path: '/adopt_main',
+              title: '認養須知'
+            },
+            {
+              path: '/adopt_dog',
+              title: '認養狗狗'
+            },
+            {
+              path: '/adopt_cat',
+              title: '認養貓貓'
+            },
+            {
+              path: '/adopt_rabbit',
+              title: '認養兔兔'
+            },
+            {
+              path: '/adopt_rodent',
+              title: '認養鼠鼠'
+            }
+          ]
+        },
+        {
+          path: '/',
+          icon: 'rabbit01.svg',
+          title: '住宿相關',
+          children: [
+            {
+              path: '/room',
+              title: '一般住宿'
+            },
+            {
+              path: '/pet_accommodation',
+              title: '毛孩寄宿'
+            }
+          ]
+        },
+        {
+          path: '/products',
+          icon: 'mouse01.svg',
+          title: '周邊商品',
+          children: []
+        }
+      ]
+    }
+  },
+
   computed: {
     // 使用对象展开运算符将 getter 混入 computed 对象中
     ...mapGetters([
---
+++ src/components/headeritem.vue
@@ -0,0 +1,9 @@
+<template>
+  <headeritem :menu="menu" />
+</template>
+
+<script>
+export default {
+  name: 'Headeritem'
+}
+</script>
